ea584a8ff8450c1144a03dc232b649917ef9408c
Art7413 <artshelbygt500@gmail.com>
2025-01-30 05:26:18 +0000

    Part 1 & 2 completed

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! \nFrom Arturo ");
+        System.out.println("Welcome to CS 112!");
+        //Static Method: Class Itself
+        sayHello();
+        //Non-static Method: Instance of Class
+        Main m = new Main();
+        m.waveGoodbye();
+    }
+
+    static void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    void waveGoodbye() {
+        System.out.println("Goodbye 👋");
     }
 }
