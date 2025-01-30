9f738d2fb99d881098fe8dfe12266feba001ae7a
Art7413 <artshelbygt500@gmail.com>
2025-01-30 05:50:50 +0000

    added hello world for part 1

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //Part 1:
+        System.out.println("Hello world! My name is Arturo.");
         System.out.println("Welcome to CS 112!");
+
         //Static Method: Class Itself
         sayHello();
+
         //Non-static Method: Instance of Class
         Main m = new Main();
         m.waveGoodbye();
     }
-
+        //Part #2
     static void sayHello() {
         System.out.println("Hello!");
     }
